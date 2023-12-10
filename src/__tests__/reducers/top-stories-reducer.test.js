a46516c95a45052ad38230122cb1461c4426c82a
Jonathan Cheng <jcheng461@gmail.com>
2023-12-10 10:19:42 -0800

    set up test1 for top stories reducer; pass test

---
+++ src/__tests__/reducers/top-stories-reducer.test.js
@@ -0,0 +1,15 @@
+import topStoriesReducer from "../../reducers/top-stories-reducer";
+
+describe("topStoriesReducer", () => {
+  const initialState = {
+    isLoaded: false,
+    topStories: [],
+    error: null,
+  };
+
+  test("should successfully throw a new error if a non-matching action type is passed into it", () => {
+    expect(() => {
+      topStoriesReducer(initialState, { type: null });
+    }).toThrow("There is no action matching null.");
+  });
+});
